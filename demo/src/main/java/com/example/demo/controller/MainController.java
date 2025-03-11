efc48868378287fa8d03c17c19c789988f2f33aa
Tyler Grim <tgrim@wisc.edu>
2025-03-11 17:11:18 -0500

    commenting

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * This class controls Get and Post calls from outside resources. It works by making a call to
+ * the specified url (hosted on 8080 by default) using /demo/... per call, then accessing the Database
+ * to either store or retrieve information.
+ */
 @Controller // This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
 public class MainController {
